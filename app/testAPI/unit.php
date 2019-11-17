287d9c29fe18839ec1c0878a80ee00eac1ee6cfe
flutterwar <paulosergioduff@gmail.com>
2019-11-17 09:52:59 -0300

    Gitflash

--- app/testAPI/unit.php
+++ app/testAPI/unit.php
@@ -1,13 +1,1 @@
-<?PHP
- 
-# PHP 7
-$conexao = mysqli_connect('localhost','root','');
-$banco = mysqli_select_db($conexao,'pessoa');
-mysqli_set_charset($conexao,'utf8');
- 
-$sql = mysqli_query($conexao,"select * from tb_pessoa") or die("Erro");
-while($dados=mysqli_fetch_assoc($sql))
-    {
-        echo $dados['nome'].'<br>';
-    }
-?>
+teste
